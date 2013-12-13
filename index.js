497d4559639f962bfa97900a433ad34432aac911
kaelzhang <i@kael.me>
2013-12-13 16:28:44 +0800

    update test cases

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 var aa = require('./lib/aa');
 var bb = require('./lib/bb');
+var c = require('c');
 
 exports.index = function () {
+    require.async('./entries/b', callback);
     console.log('index.index', aa.aa(), bb.bb());
 };
